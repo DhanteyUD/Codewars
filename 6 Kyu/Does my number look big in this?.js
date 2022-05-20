bd2b9cc3195a5bb0baf1122df43e61f11beb5b69
DhanteyUD <otseclinton60@gmail.com>
2022-05-20 05:13:50 +0100

    Does my number look big in this?

--- 6 Kyu/Does my number look big in this?.js
+++ 6 Kyu/Does my number look big in this?.js
@@ -1,5 +1,5 @@
-// A Narcissistic Number is a positive number which is the sum of its own digits, 
-// each raised to the power of the number of digits in a given base. 
+// A Narcissistic Number is a positive number which is the sum of its own digits,
+// each raised to the power of the number of digits in a given base.
 // In this Kata, we will restrict ourselves to decimal (base 10).
 
 // For example, take 153 (3 digits), which is narcisstic:
@@ -11,23 +11,23 @@
 
 // The Challenge:
 
-// Your code must return true or false (not 'true' and 'false') 
-// depending upon whether the given number is a Narcissistic number in base 10. 
+// Your code must return true or false (not 'true' and 'false')
+// depending upon whether the given number is a Narcissistic number in base 10.
 // This may be True and False in your language, e.g. PHP.
 
-// Error checking for text strings or other invalid inputs is not required, 
+// Error checking for text strings or other invalid inputs is not required,
 // only valid positive non-zero integers will be passed into the function.
 
 function narcissistic(value) {
   return (
     value
       .toString()
-      .split("")
+      .split('')
       .map((v, i, arr) => Math.pow(arr[i], arr.length))
       .reduce((a, b) => a + b, 0) === value
-  ) ? 
+  );
 }
 
-console.log(narcissistic(153))
+console.log(narcissistic(153));
 // narcissistic( 7 ), true, "7 is narcissistic"
 // narcissistic( 371 ), true, "371 is narcissistic"

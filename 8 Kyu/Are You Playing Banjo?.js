a12091a3c32214f15265d7f1e447dd7dbe321d49
DhanteyUD <otseclinton60@gmail.com>
2022-06-04 15:15:39 +0100

    Are You Playing Banjo?

---
+++ 8 Kyu/Are You Playing Banjo?.js
@@ -0,0 +1,24 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+  if (name[0] === 'R' || name[0] === 'r') {
+    return name + ' plays banjo';
+  }
+  return name + ' does not play banjo';
+}
+
+console.log(areYouPlayingBanjo('Adam'));
+// to return "Adam does not play banjo"
+
+console.log(areYouPlayingBanjo('Paul'));
+// to return "Paul does not play banjo"
+
+console.log(areYouPlayingBanjo('Ringo'));
+// to return "Ringo plays banjo"

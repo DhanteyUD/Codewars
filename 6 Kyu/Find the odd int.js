fd34674bbbeeb82a5662af03cdb81cc08bd5f96e
DhanteyUD <otseclinton60@gmail.com>
2022-07-05 13:42:56 +0100

    Find the odd int - updated

--- 6 Kyu/Find the odd int.js
+++ 6 Kyu/Find the odd int.js
@@ -9,14 +9,20 @@
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
+// // Solution 1
+// function findOdd(A) {
+//   let odd = 0;
+//   A.forEach(num => {
+//     odd ^= num;
+//   });
+//   return odd;
+// }
+
+// Solution 2
 function findOdd(A) {
-  let odd = 0;
-  A.forEach(num => {
-    odd ^= num;
-  });
-  return odd;
+  return A.find((item) => A.filter(el => el == item).length % 2)
 }
 
 console.log(findOdd([1,1,2])); // 2
-//console.log(findOdd([0,1,0,1,0])); // 0
-//console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1])); // 4
+console.log(findOdd([0,1,0,1,0])); // 0
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1])); // 4

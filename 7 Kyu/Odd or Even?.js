368ccdf6bd238a975bbaf1a5cc3e4285f85bb30e
DhanteyUD <otseclinton60@gmail.com>
2022-08-16 09:37:37 +0100

    Odd or Even

--- 7 Kyu/Odd or Even?.js
+++ 7 Kyu/Odd or Even?.js
@@ -17,3 +17,11 @@ Input: [0, -1, -5]
 Output: "even"
 
 */
+
+function oddOrEven(array) {
+  return array.reduce((a, b) => a + b, 0) % 2 === 0 ? 'even' : 'odd';
+}
+
+console.log(oddOrEven([0])) // 'even'
+console.log(oddOrEven([0, 1, 4])) // 'odd'
+console.log(oddOrEven([0, -1, -5])) // 'even'

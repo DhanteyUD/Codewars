2297f8fb042249f197059eef444b2d44b50156a1
DhanteyUD <otseclinton60@gmail.com>
2022-08-17 14:48:46 +0100

    Calculate BMI

--- 7 Kyu/Calculate BMI.js
+++ 7 Kyu/Calculate BMI.js
@@ -1,17 +1,26 @@
-/* 
-Write function bmi that calculates body mass index (bmi = weight / height2).
-
-if bmi <= 18.5 return "Underweight"
-
-if bmi <= 25.0 return "Normal"
-
-if bmi <= 30.0 return "Overweight"
-
-if bmi > 30 return "Obese"
-*/
+/**
+ * /*
+ * Write function bmi that calculates body mass index (bmi = weight / height2).
+ *
+ * if bmi <= 18.5 return "Underweight"
+ *
+ * if bmi <= 25.0 return "Normal"
+ *
+ * if bmi <= 30.0 return "Overweight"
+ *
+ * if bmi > 30 return "Obese"
+ *
+ * @format
+ */
 
 function bmi(weight, height) {
-  return "";
+  return weight / height ** 2 <= 18.5
+    ? 'Underweight'
+    : weight / height ** 2 <= 25.0
+    ? 'Normal'
+    : weight / height ** 2 <= 30.5
+    ? 'Overweight'
+    : 'Obese';
 }
 
-console.log(bmi(80, 1.80)); // "Normal"
+console.log(bmi(80, 1.8)); // "Normal"

59df93c72bc61ea3910fc9c302dc33438976f9f0
DhanteyUD <otseclinton60@gmail.com>
2022-09-18 14:37:00 +0100

    Roman Numerals Helper

--- 4 Kyu/Roman Numerals Helper.js
+++ 4 Kyu/Roman Numerals Helper.js
@@ -23,7 +23,7 @@
  * Help
  * Symbol	Value
  * I	        1
- * IV	      4
+ * IV	        4
  * V	        5
  * X	        10
  * L	        50
@@ -33,3 +33,54 @@
  *
  * @format
  */
+
+ const RomanNumerals = {
+  toRoman: (number) => {
+    let result = "",
+      decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+      roman = [
+        "M",
+        "CM",
+        "D",
+        "CD",
+        "C",
+        "XC",
+        "L",
+        "XL",
+        "X",
+        "IX",
+        "V",
+        "IV",
+        "I",
+      ];
+
+    decimals.map(function (value, index) {
+      while (number >= value) {
+        result += roman[index];
+        number -= value;
+      }
+    });
+
+    return result;
+  },
+  fromRoman: (roman) => {
+    const conversion = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    };
+    return roman
+      .match(/CM|CD|XC|XL|IX|IV|\w/g)
+      .reduce((accum, roman) => accum + conversion[roman], 0);
+  },
+};

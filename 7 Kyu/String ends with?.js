4ccdcb4d4800d8a58b12b302d924b9f8f8a6e2db
DhanteyUD <otseclinton60@gmail.com>
2022-09-26 18:10:57 +0100

    String Ends With

--- 7 Kyu/String ends with?.js
+++ 7 Kyu/String ends with?.js
@@ -11,3 +11,10 @@
  *
  * @format
  */
+
+ function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+console.log(solution('abcde', 'cde')) // true
+console.log(solution('abcde', 'abc')) // false

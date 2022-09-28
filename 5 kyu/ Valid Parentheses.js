cba04339f3a200f610ad9e925a372fb9d9adbf41
DhanteyUD <otseclinton60@gmail.com>
2022-09-28 23:23:24 +0100

    Valid Parentheses - More Possible Solutions

--- 5 kyu/ Valid Parentheses.js
+++ 5 kyu/ Valid Parentheses.js
@@ -16,6 +16,7 @@
  * @format
  */
 
+//* Option 1
 function validParentheses(parens) {
   let count = 0;
   for (let i = 0; i < parens.length; i++) {
@@ -24,6 +25,44 @@ function validParentheses(parens) {
     if (count < 0) return false;
   }
   return count === 0;
+}
+
+//* Option 2
+function validParentheses(parens) {
+  var re = /\(\)/;
+  while (re.test(parens)) parens = parens.replace(re, '');
+  return !parens;
+}
+
+//* Option 3
+function validParentheses(parens) {
+  var stack = [],
+    i;
+
+  for (i = 0; i < parens.length; i++) {
+    if (parens[i] === '(') {
+      stack.push(parens[i]);
+    } else if ('(' !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+}
+
+//* Option 4
+function validParentheses(v) {
+  while (v.includes('()')) {
+    v = v.replace('()', '');
+  }
+  return v.length === 0;
+}
+
+//* Option 5
+function validParentheses(parens) {
+  return [...parens].reduce((a, c) => (a + c).replace('()', '')) == ''
+    ? true
+    : false;
 }
 
 console.log(validParentheses('()'), true);

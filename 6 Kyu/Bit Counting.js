83340a45d5b815359abf8dafaa6e78939a7ab808
DhanteyUD <otseclinton60@gmail.com>
2022-10-02 19:01:43 +0100

    Bit Counting

--- 6 Kyu/Bit Counting.js
+++ 6 Kyu/Bit Counting.js
@@ -1,15 +1,18 @@
 /**
  * /*
- * Write a function that takes an integer as input, and returns the 
- * number of bits that are equal to one in the binary representation of 
+ * Write a function that takes an integer as input, and returns the
+ * number of bits that are equal to one in the binary representation of
  * that number. You can guarantee that input is non-negative.
  *
- * Example: The binary representation of 1234 is 10011010010, so the 
+ * Example: The binary representation of 1234 is 10011010010, so the
  * function should return 5 in this case
  *
  * @format
  */
 
- var countBits = function(n) {
-  // Program Me
+var countBits = function (n) {
+  return n.toString(2).replace(/0/g, '').length;
 };
+
+console.log(countBits(0)); // 0
+console.log(countBits(4)); // 1

64eeb7728d7c2e002c36fd2640f9b84041c1a354
DhanteyUD <otseclinton60@gmail.com>
2022-10-04 17:31:29 +0100

    Weight for Weight

--- 5 kyu/Weight for weight.js
+++ 5 kyu/Weight for weight.js
@@ -1,42 +1,79 @@
 /**
- * 
- * My friend John and I are members of the "Fat to Fit Club (FFC)". 
- * John is worried because each month a list with the weights of members 
- * is published and each month he is the last on the list which means he is 
+ *
+ * My friend John and I are members of the "Fat to Fit Club (FFC)".
+ * John is worried because each month a list with the weights of members
+ * is published and each month he is the last on the list which means he is
  * the heaviest.
  *
  * I am the one who establishes the list so I told him: "Don't worry any more,
- * I will modify the order of the list". 
- * It was decided to attribute a "weight" to numbers. 
+ * I will modify the order of the list".
+ * It was decided to attribute a "weight" to numbers.
  * The weight of a number will be from now on the sum of its digits.
  *
- * For example 99 will have "weight" 18, 100 will have "weight" 1 so in the 
+ * For example 99 will have "weight" 18, 100 will have "weight" 1 so in the
  * list 100 will come before 99.
  *
- * Given a string with the weights of FFC members in normal order can you 
+ * Given a string with the weights of FFC members in normal order can you
  * give this string ordered by "weights" of these numbers?
  *
  * Example:
  * "56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes:
  *
  * "100 180 90 56 65 74 68 86 99"
- * When two numbers have the same "weight", let us class them as if they 
+ * When two numbers have the same "weight", let us class them as if they
  * were strings (alphabetical ordering) and not numbers:
  *
- * 180 is before 90 since, having the same "weight" (9), it comes before as 
+ * 180 is before 90 since, having the same "weight" (9), it comes before as
  * a string.
  *
  * All numbers in the list are positive numbers and the list can be empty.
  *
  * Notes
- * it may happen that the input string have leading, trailing whitespaces 
+ * it may happen that the input string have leading, trailing whitespaces
  * and more than a unique whitespace between two consecutive numbers
- * 
+ *
  * For C: The result is freed.
  *
  * @format
  */
 
- function orderWeight(strng) {
-  // your code
+// Option 1
+function orderWeight(strng) {
+  let arr = strng.split(' ');
+  let result = arr.sort((a, b) => {
+    let sumA = a.split('').reduce((acc, cur) => acc + Number(cur), 0);
+    let sumB = b.split('').reduce((acc, cur) => acc + Number(cur), 0);
+    if (sumA === sumB) {
+      return a.localeCompare(b);
+    }
+    return sumA - sumB;
+  });
+  return result.join(' ');
 }
+
+// Option 2
+function orderWeight(str) {
+  const sum = (x) => x.split('').reduce((res, cur) => res + +cur, 0);
+  return str
+    .split(' ')
+    .sort((a, b) => {
+      const diff = sum(a) - sum(b);
+      return diff == 0 ? (a > b ? 1 : -1) : diff;
+    })
+    .join(' ');
+}
+
+// Option 3
+function orderWeight(s) {
+  return s
+    .split(' ')
+    .sort(
+      (a, b) =>
+        a.split('').reduce((pv, cv) => pv + +cv, 0) -
+          b.split('').reduce((pv, cv) => pv + +cv, 0) || (a > b ? 1 : -1)
+    )
+    .join(' ');
+}
+
+console.log(orderWeight('103 123 4444 99 2000')); // "2000 103 123 4444 99"
+console.log(orderWeight('2000 10003 1234000 44444444 9999 11 11 22 123')); // "11 11 2000 10003 22 123 1234000 44444444 9999"

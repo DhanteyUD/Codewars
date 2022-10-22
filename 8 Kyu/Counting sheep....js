4aa8fb66318ff4b716c6e6d8391294203b37e479
DhanteyUD <otseclinton60@gmail.com>
2022-10-22 12:42:25 +0100

    Counting sheep...  - Options

--- 8 Kyu/Counting sheep....js
+++ 8 Kyu/Counting sheep....js
@@ -1,20 +1,23 @@
-/*
-Consider an array/list of sheep where some sheep may be missing from their 
-place. We need a function that counts the number of sheep present in the 
-array (true means present).
-
-For example,
-
-[true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true]
-The correct answer would be 17.
-
-Hint: Don't forget to check for bad values like null/undefined
-*/ 
+/**
+ * /*
+ * Consider an array/list of sheep where some sheep may be missing from their
+ * place. We need a function that counts the number of sheep present in the
+ * array (true means present).
+ *
+ * For example,
+ *
+ * [true,  true,  true,  false,
+ *   true,  true,  true,  true ,
+ *   true,  false, true,  false,
+ *   true,  false, false, true ,
+ *   true,  true,  true,  true ,
+ *   false, false, true,  true]
+ * The correct answer would be 17.
+ *
+ * Hint: Don't forget to check for bad values like null/undefined
+ *
+ * @format
+ */
 
 // Option 1
 function countSheeps(arrayOfSheep) {
@@ -22,3 +25,32 @@ function countSheeps(arrayOfSheep) {
   return result.length;
 }
 
+// Option 2
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
+}
+
+// Option 3
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  var num = 0;
+
+  for (var i = 0; i < arrayOfSheep.length; i++)
+    if (arrayOfSheep[i] == true) num++;
+
+  return num;
+}
+
+// Option 4
+function countSheeps(arrayOfSheep) {
+  var count = 0;
+
+  arrayOfSheep.forEach(function (sheep) {
+    if (sheep) count++;
+  });
+
+  return count;
+}
+
+// Option 5
+const countSheeps = (arrayOfSheeps) => arrayOfSheeps.filter((s) => s).length;

b9928af24b6ecb175aa3121a25254a6378ee0c5c
DhanteyUD <otseclinton60@gmail.com>
2022-10-24 16:13:05 +0100

    Growth of a Population

--- 7 Kyu/Growth of a Population.js
+++ 7 Kyu/Growth of a Population.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * In a small town the population is p0 = 1000 at the beginning of a year.
  * The population regularly increases by 2 percent per year and moreover 50 new
  * inhabitants per year come to live in the town.
@@ -38,11 +38,22 @@
  * @format
  */
 
-function nbYear(p0, percent, aug, p) {
-  let year = 0;
-  while (p0 < p) {
-    year++;
-    p0 = p0 + p0 * (percent / 100) + aug;
-  }
-  return year;
+// Option 1
+ function nbYear(p0, percent, aug, p, years = 0) {
+  return p0 >= p
+     ? years
+     : nbYear(
+         Math.floor(p0 + p0 * (percent / 100)) + aug,
+         percent,
+         aug,
+         p,
+         years + 1
+       );
 }
+
+
+
+console.log(nbYear(1500, 5, 100, 5000)); // 15
+console.log(nbYear(1500000, 2.5, 10000, 2000000)); // 10
+console.log(nbYear(1500000, 0.25, 1000, 2000000)); // 94
+console.log(nbYear(1000, 0.02, 50, 1200)); // 3

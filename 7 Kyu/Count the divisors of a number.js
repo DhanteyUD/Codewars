62da32f255d27b5f946cae2ab7ccc09c1fd20491
DhanteyUD <otseclinton60@gmail.com>
2022-10-29 13:08:28 +0100

    Count the divisors of a number

--- 7 Kyu/Count the divisors of a number.js
+++ 7 Kyu/Count the divisors of a number.js
@@ -14,5 +14,17 @@ numbers are counted in each case.
 */ 
 
 function getDivisorsCnt(n){
-  // todo
+  let count = 0;
+  for (let i=0; i<=n; i++) {
+    if (n % i === 0) {
+      count++
+    }
+    count
+  }
+  return count
 }
+
+console.log(getDivisorsCnt(1)) // 1
+console.log(getDivisorsCnt(10)) // 4
+console.log(getDivisorsCnt(11)) // 2
+console.log(getDivisorsCnt(54)) // 8

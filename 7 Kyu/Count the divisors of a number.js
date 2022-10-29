44b8b3d4cf391c119352f9920098fcdc4ff97882
DhanteyUD <otseclinton60@gmail.com>
2022-10-29 13:10:24 +0100

    Count the divisors of a number

--- 7 Kyu/Count the divisors of a number.js
+++ 7 Kyu/Count the divisors of a number.js
@@ -1,30 +1,32 @@
-/*
-Count the number of divisors of a positive integer n.
+/**
+ * Count the number of divisors of a positive integer n.
+ *
+ * Random tests go up to n = 500000.
+ *
+ * Examples (input --> output)
+ * 4 --> 3 (1, 2, 4)
+ * 5 --> 2 (1, 5)
+ * 12 --> 6 (1, 2, 3, 4, 6, 12)
+ * 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+ * Note you should only return a number, the count of divisors.
+ * The numbers between parentheses are shown only for you to see which
+ * numbers are counted in each case.
+ *
+ * @format
+ */
 
-Random tests go up to n = 500000.
-
-Examples (input --> output)
-4 --> 3 (1, 2, 4)
-5 --> 2 (1, 5)
-12 --> 6 (1, 2, 3, 4, 6, 12)
-30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
-Note you should only return a number, the count of divisors. 
-The numbers between parentheses are shown only for you to see which 
-numbers are counted in each case.
-*/ 
-
-function getDivisorsCnt(n){
+function getDivisorsCnt(n) {
   let count = 0;
-  for (let i=0; i<=n; i++) {
+  for (let i = 0; i <= n; i++) {
     if (n % i === 0) {
-      count++
+      count++;
     }
-    count
+    count;
   }
-  return count
+  return count;
 }
 
-console.log(getDivisorsCnt(1)) // 1
-console.log(getDivisorsCnt(10)) // 4
-console.log(getDivisorsCnt(11)) // 2
-console.log(getDivisorsCnt(54)) // 8
+console.log(getDivisorsCnt(1)); // 1
+console.log(getDivisorsCnt(10)); // 4
+console.log(getDivisorsCnt(11)); // 2
+console.log(getDivisorsCnt(54)); // 8

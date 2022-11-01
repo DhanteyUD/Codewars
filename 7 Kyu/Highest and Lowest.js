4cd747ceba7f4447a8c37866bab9afcdbf3f6520
DhanteyUD <otseclinton60@gmail.com>
2022-11-01 10:36:42 +0100

    Highest and Lowest - Options

--- 7 Kyu/Highest and Lowest.js
+++ 7 Kyu/Highest and Lowest.js
@@ -13,6 +13,8 @@ Output string must be two numbers separated by a single space,
 and highest number is first.
 */ 
 
+
+// Option 1
 function highAndLow(numbers){
   let arr = numbers.split(' ').map(Number);
   return `${Math.max(...arr)} ${Math.min(...arr)}`;
@@ -20,3 +22,32 @@ function highAndLow(numbers){
 
 console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")) // "42 -9"
 console.log(highAndLow("1 2 3")) // "3 1"
+
+
+// Option 2
+function highAndLow(numbers){
+  numbers = numbers.split(' ').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
+
+// Option 3
+function highAndLow(numbers) {
+  var arr = numbers.split(" ");
+  return Math.max.apply(null, arr) + ' ' + Math.min.apply(null, arr);
+}
+
+// Option 4
+function highAndLow(numbers){
+  var arr = numbers.split(' ').sort(function(a, b) { return a - b });
+  return arr[arr.length -1] + ' ' + arr[0];
+}
+
+// Option 5
+function highAndLow(numbers) {
+  numbers = numbers.split(' ').map(Number)
+  
+  var min = Math.min.apply(null, numbers)
+  var max = Math.max.apply(null, numbers)
+  
+  return max + ' ' + min
+}

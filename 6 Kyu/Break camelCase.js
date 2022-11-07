de66b510b00627906b6ec290dc2586bc8c49307a
DhanteyUD <otseclinton60@gmail.com>
2022-11-07 13:53:29 +0100

    Break camelCase - Options

--- 6 Kyu/Break camelCase.js
+++ 6 Kyu/Break camelCase.js
@@ -11,7 +11,7 @@
  * @format
  */
 
-// complete the function
+// Option 1
 function solution(string) {
   return result = string.split("").map((x) => {
     if (x === x.toUpperCase()) {
@@ -21,6 +21,26 @@ function solution(string) {
     }
   }
   ).join("");
+}
+
+// Option 2
+function solution(string) {
+  return(string.replace(/([A-Z])/g, ' $1'));
+}
+
+// Option 3
+function solution(string) {
+  return string.replace(/([a-z])([A-Z])/g, "$1 $2");
+}
+
+// Option 4
+function solution(string){
+  return string.replace(/[A-Z]/g, function(c){return " " + c;});
+}
+
+// Option 5
+function solution(string) {
+  return string.replace(/(?=[A-Z])/g," ")
 }
 
 console.log(solution('camelCasing')); // "camel Casing"

90c667357787b89c742b30ac5ccc13a5617dd12e
DhanteyUD <otseclinton60@gmail.com>
2022-11-07 12:53:30 +0100

    Break camelCase

--- 6 Kyu/Break camelCase.js
+++ 6 Kyu/Break camelCase.js
@@ -1,14 +1,28 @@
-/*
-Complete the solution so that the function will break up camel casing, 
-using a space between words.
-
-Example
-"camelCasing"  =>  "camel Casing"
-"identifier"   =>  "identifier"
-""             =>  ""
-*/ 
+/**
+ *
+ * Complete the solution so that the function will break up camel casing,
+ * using a space between words.
+ *
+ * Example
+ * "camelCasing"  =>  "camel Casing"
+ * "identifier"   =>  "identifier"
+ * ""             =>  ""
+ *
+ * @format
+ */
 
 // complete the function
 function solution(string) {
-    
+  return result = string.split("").map((x) => {
+    if (x === x.toUpperCase()) {
+      return " " + x;
+    } else {
+      return x;
+    }
+  }
+  ).join("");
 }
+
+console.log(solution('camelCasing')); // "camel Casing"
+console.log(solution('identifier')); // "identifier"
+console.log(solution('')); // ""

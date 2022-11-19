b21cfd2fc1bc61413dfaa02e233a3a053d34ae9e
DhanteyUD <otseclinton60@gmail.com>
2022-11-19 11:34:02 +0100

    Frog Jumping

--- 6 Kyu/Frog jumping.js
+++ 6 Kyu/Frog jumping.js
@@ -21,5 +21,16 @@ All tests for this Kata are randomly generated.
 */ 
 
 function solution(a) {
-  // Your code here
+  if (a.length === 0) return -1;
+  let count = 0;
+  for (let i = 0; i < a.length; i += a[i]) {
+    if (i < 0) {
+      break;
+    }
+    count++;
+    if (count > 1000) {
+      return -1;
+    }
+  }
+  return count;
 }

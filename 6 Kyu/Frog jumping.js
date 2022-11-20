1518871b13a490ddb5dd682ecb82a7e9934511d4
DhanteyUD <otseclinton60@gmail.com>
2022-11-20 16:48:46 +0100

    Frog Jumping - Options

--- 6 Kyu/Frog jumping.js
+++ 6 Kyu/Frog jumping.js
@@ -20,6 +20,7 @@ jumps = 3  (1 -> 2 -> 5 -> <jump out>)
 All tests for this Kata are randomly generated.
 */ 
 
+// Option 1
 function solution(a) {
   if (a.length === 0) return -1;
   let count = 0;
@@ -33,4 +34,67 @@ function solution(a) {
     }
   }
   return count;
+}
+
+// Option 2
+function solution(a) {
+  var c = 0, i = 0;
+
+  while (typeof a[i] !== 'undefined') {
+    c++;
+    i += a[i];
+    
+    if (c > a.length) {
+      return -1;
+    }
+  }
+
+  return a.length ? c : -1;
+}
+
+// Option 3
+function solution(a) {
+  for (var s = [], c = i = q = 0; i < a.length && !q; c += !!(a[i])) {
+    if (!(q = s.indexOf(i += a[i]) > -1)) s.push(i);
+  }
+  return a.length && !q ? ++c : -1;
+}
+
+// Option 4
+function solution(a) {
+  var steps = 0;
+  var visited = [];
+
+  var i = 0;
+  while (true) {
+    steps++;
+    
+    // Check for escape
+    if (i + a[i] > a.length - 1 || i + a[i] < 0) {
+      break;
+    }
+    // Check for revisited stone or no stones at all
+    else if (visited.indexOf(i) >= 0 || a.length == 0) {
+      steps = -1;
+      break;
+    }
+    // otherwise jump!
+    else {
+      // Keep track of the stones the frog
+      // has been to, so if it revists one, 
+      // we know it's stuck.
+      visited.push(i);
+      
+      // Then move to the new stone
+      i += a[i];
+    }
+  }
+  return steps;
+}
+
+// Option 5
+function solution(a) {
+  var steps = 0, visited = [], i = 0, l = a.length;
+  while (i < l && i >= 0 && !visited[i]) { steps++; visited[i] = true; i += a[i]; }
+  return (visited[i] || !l) ? -1 : steps;
 }

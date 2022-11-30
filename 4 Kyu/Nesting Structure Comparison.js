057e36aeb8749ff4d72748b5f6d6f70efcb7b40e
DhanteyUD <otseclinton60@gmail.com>
2022-11-30 20:06:48 +0100

    Nesting Structure Comparison - Option 2

--- 4 Kyu/Nesting Structure Comparison.js
+++ 4 Kyu/Nesting Structure Comparison.js
@@ -1,27 +1,31 @@
-/*
-Complete the function/method (depending on the language) to return true/True 
-when its argument is an array that has the same nesting structures and same 
-corresponding length of nested arrays as the first array.
+/**
+ * /*
+ * Complete the function/method (depending on the language) to return true/True
+ * when its argument is an array that has the same nesting structures and same
+ * corresponding length of nested arrays as the first array.
+ *
+ * For example:
+ *
+ *  // should return true
+ * [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );
+ * [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] );
+ *
+ *  // should return false
+ * [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] );
+ * [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] );
+ *
+ * // should return true
+ * [ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] );
+ *
+ * // should return false
+ * [ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] );
+ * For your convenience, there is already a function 'isArray(o)' declared and
+ * defined that returns true if its argument is an array, false otherwise.
+ *
+ * @format
+ */
 
-For example:
-
- // should return true
-[ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );          
-[ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] );  
-
- // should return false 
-[ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] );  
-[ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] );  
-
-// should return true
-[ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] ); 
-
-// should return false
-[ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] );     
-For your convenience, there is already a function 'isArray(o)' declared and 
-defined that returns true if its argument is an array, false otherwise.
-*/ 
-
+// Option 1
 Array.prototype.sameStructureAs = function (other) {
   if (!Array.isArray(other) || this.length != other.length) return false;
 
@@ -36,4 +40,13 @@ Array.prototype.sameStructureAs = function (other) {
   }
 
   return true;
+};
+
+// Option 2
+Array.prototype.sameStructureAs = function (other) {
+  return this.length === other.length
+    ? this.every(function (el, i) {
+        return Array.isArray(el) ? el.sameStructureAs(other[i]) : true;
+      })
+    : false;
 };

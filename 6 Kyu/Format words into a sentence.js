4a0b29af79c344da4c1b17212e841e1cb1f296cd
DhanteyUD <otseclinton60@gmail.com>
2023-01-21 15:48:37 +0100

    Format words into a sentence

--- 6 Kyu/Format words into a sentence.js
+++ 6 Kyu/Format words into a sentence.js
@@ -1,21 +1,28 @@
-/*
-Complete the method so that it formats the words into a single comma 
-separated value. The last word should be separated by the word 'and' 
-instead of a comma. The method takes in an array of strings and returns a 
-single formatted string.
+/**
+ * /*
+ * Complete the method so that it formats the words into a single comma
+ * separated value. The last word should be separated by the word 'and'
+ * instead of a comma. The method takes in an array of strings and returns a
+ * single formatted string.
+ *
+ * Note:
+ *
+ * Empty string values should be ignored.
+ * Empty arrays or null/nil/None values being passed into the method should
+ * result in an empty string being returned.
+ * Example: (Input --> output)
+ *
+ * ['ninja', 'samurai', 'ronin'] --> "ninja, samurai and ronin"
+ * ['ninja', '', 'ronin'] --> "ninja and ronin"
+ * [] -->""
+ *
+ * @format
+ */
 
-Note:
-
-Empty string values should be ignored.
-Empty arrays or null/nil/None values being passed into the method should 
-result in an empty string being returned.
-Example: (Input --> output)
-
-['ninja', 'samurai', 'ronin'] --> "ninja, samurai and ronin"
-['ninja', '', 'ronin'] --> "ninja and ronin"
-[] -->""
-*/ 
-
-function formatWords(words){
-  
+function formatWords(words) {
+  if (!words) return '';
+  words = words.filter((v) => v.length > 0);
+  if (words.length === 0) return '';
+  if (words.length === 1) return words[0];
+  return words.slice(0, -1).join(', ') + ' and ' + words.slice(-1);
 }

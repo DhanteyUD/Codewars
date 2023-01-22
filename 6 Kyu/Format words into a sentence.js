3480e3e06ff7ecd90e5a4592c3ef710ed28cfce2
DhanteyUD <otseclinton60@gmail.com>
2023-01-22 13:48:26 +0100

    Format words into a sentence - Options

--- 6 Kyu/Format words into a sentence.js
+++ 6 Kyu/Format words into a sentence.js
@@ -19,6 +19,7 @@
  * @format
  */
 
+// Option 1
 function formatWords(words) {
   if (!words) return '';
   words = words.filter((v) => v.length > 0);
@@ -26,3 +27,51 @@ function formatWords(words) {
   if (words.length === 1) return words[0];
   return words.slice(0, -1).join(', ') + ' and ' + words.slice(-1);
 }
+
+// Option 2
+function formatWords(words) {
+  if (!words) return '';
+  return words
+    .filter(function (a) {
+      return a !== '';
+    })
+    .join(', ')
+    .replace(/(, )+(\S+)$/, ' and $2');
+}
+
+// Option 3
+function formatWords(words) {
+  if (!words || words.length === 0) {
+    return '';
+  }
+  words = words.filter(function (word) {
+    return word.length;
+  });
+  if (words.length == 0) {
+    return '';
+  } else if (words.length == 1) {
+    return words[0];
+  } else {
+    return [
+      words.slice(0, words.length - 1).join(', '),
+      words.slice(words.length - 1)[0],
+    ].join(' and ');
+  }
+}
+
+// Option 4
+function formatWords(a) {
+  return (a || [])
+    .filter((x) => x)
+    .join(', ')
+    .replace(/,(?= [^,]*$)/, ' and');
+}
+
+// Option 5
+const formatWords = (w) =>
+  w
+    ? w
+        .filter((e) => e)
+        .join(', ')
+        .replace(/,(?=\s\w+$)/g, ' and')
+    : '';

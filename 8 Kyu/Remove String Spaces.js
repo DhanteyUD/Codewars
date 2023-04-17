70aa1b786c85b212fb434ba0993200be964d8601
Clinton Otse <clinton.otse@outsourceglobal.com>
2023-04-17 20:59:17 +0100

    Initial Clone

--- 8 Kyu/Remove String Spaces.js
+++
@@ -1,9 +0,0 @@
-// Simple, remove the spaces from the string, then return the resultant string.
-
-function noSpace(x) {
-  return x.split(' ').join('');
-}
-
-console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
-
-// should return '8j8mBliB8gimjB8B8jlB'

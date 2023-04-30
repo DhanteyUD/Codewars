45c179cead186d1f4682c149845f8bede9c184da
Clinton Otse <clinton.otse@outsourceglobal.com>
2023-04-30 18:42:56 +0100

    Find the stray number

--- 7 Kyu/Find the stray number.js
+++ 7 Kyu/Find the stray number.js
@@ -13,5 +13,11 @@ Examples
 */ 
 
 function stray(numbers) {
-    return 0;
+    let strayNumber = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+            strayNumber = numbers[i];
+        }
+    }
+    return strayNumber;
 }
